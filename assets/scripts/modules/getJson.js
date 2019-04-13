27611a19eea38de55ceb636c1df170c290a719d8
Mondal10 <amitmondal111.am@gmail.com>
2019-04-13 15:19:53 +0530

    using fetch() and color change

--- assets/scripts/modules/getJson.js
+++ assets/scripts/modules/getJson.js
@@ -9,15 +9,28 @@ import { createKeys } from './createKeys.js'
  */
 
 export const getJsonData = (url) => {
+    // console.time('Time to load')
+    // $.ajax({
+    //     dataType: "json",
+    //     url: url,
+    //     // data: data,
+    //     async: true, // true for async request ('false' for sync request, which can get deprecated)
+    //     success: (data) => {
+    //         // Object.assign(configData, data);
+    //         createKeys(data);
+    //         console.timeEnd('Time to load');
+    //     }
+    // });
 
-    $.ajax({
-        dataType: "json",
-        url: url,
-        // data: data,
-        async: true, // true for async request ('false' for sync request, which can get deprecated)
-        success: (data) => {
-            // Object.assign(configData, data);
+    // Trying to use fetch although fetch, $.ajax and $.getJSON are sort of similar
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
             createKeys(data);
-        }
-    });
+            // console.timeEnd('Time to load')
+        })
+
+
 }
